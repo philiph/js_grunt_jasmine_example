edba0b825e3f57ea48b6ac9444d248f6fba254b0
Philip Hagen <phil@philiphagen.com>
2014-05-20 07:56:16 +0800

    Configure Jasmine tests in gruntfile.

--- gruntfile.js
+++ gruntfile.js
@@ -1,9 +1,18 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-
+    jasmine: {
+      pivotal: {
+        src: 'src/*.js',
+        options: {
+          specs: 'tests/*.spec.js'
+        }
+      }
+    }
   });
 
-  grunt.registerTask('default', []);
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+  grunt.registerTask('default', ['jasmine']);
 
 };
