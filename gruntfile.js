1f271f4b0094406eaab4e1a08b133e500bbd8426
Philip Hagen <phil@philiphagen.com>
2014-05-20 19:31:14 +0800

    Added jshint to default grunt task.

--- gruntfile.js
+++ gruntfile.js
@@ -17,6 +17,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask('default', ['jasmine']);
+  grunt.registerTask('default', ['jshint', 'jasmine']);
 
 };
